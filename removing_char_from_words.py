1e854350500b19002759f43842ad9e216208965d
Marck Christian Agulto <150341453+m1sskeiso@users.noreply.github.com>
2024-01-22 03:00:29 +0700

    Second

--- removing_char_from_words.py
+++ removing_char_from_words.py
@@ -1,12 +1,17 @@
 # Define a function remove characters
 def remove_chars(input_string, n):
-   
+
+    
     # Check if n is less than the length of the string
-    result = input_string[n:]
-    return result
+    if n < len(input_string)
 
 # Remove characters from the start up to n and return the new string
+        result = input_string[n:]
+        return result
+
 # If n is greater than or equal to the length, return an error message
+    else:
+         return "Error: n must be less than the length of the string."
 # Test cases
 
 
