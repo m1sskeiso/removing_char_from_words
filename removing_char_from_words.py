febc453ae22f4094d3418d54ac7451df6d572d24
Marck Christian Agulto <150341453+m1sskeiso@users.noreply.github.com>
2024-01-22 02:53:10 +0700

    pseudocode

---
+++ removing_char_from_words.py
@@ -0,0 +1,7 @@
+# Define a function remove characters
+# Check if n is less than the length of the string
+# Remove characters from the start up to n and return the new string
+# If n is greater than or equal to the length, return an error message
+# Test cases
+
+
